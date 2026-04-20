7d86c67006ae1779d8bdc7af05ac8b4be730d8ba
Amans2801 <aman786saxena@gmail.com>
2026-04-20 23:29:42 +0530

    Add project files.

---
+++ Core/DTO's/Requests/AdduserRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace HealthyFyMe.Core.DTO_s.Requests
+{
+    public class AddUserRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password {  get; set; } = string.Empty;
+
+        public string Goal { get; set; } = string.Empty;
+
+        public decimal Height { get; set; }
+
+        public decimal Weight { get; set; }
+    }
+}
---
+++ Core/Db/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿namespace HealthyFyMe.Core.Db.Models
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string MobileNumber { get; set; } = string.Empty;
+
+        public string HashPassword { get; set; } = string.Empty;
+
+        public string Goal { get; set; } = string.Empty;
+
+        public decimal Height { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
